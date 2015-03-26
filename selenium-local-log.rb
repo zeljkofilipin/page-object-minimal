7146cf69f87b9dc41105aa93d1d8cd47c0ad53eb
Željko Filipin <zeljko.filipin@gmail.com>
2015-03-26 18:41:08 +0100

    Firefox log work
    Thanks, Dan.

--- selenium-local-log.rb
+++ selenium-local-log.rb
@@ -1,7 +1,7 @@
 require "selenium-webdriver"
 
 profile = Selenium::WebDriver::Firefox::Profile.new
-profile["webdriver.firefox.logfile"] = "/Users/z/Desktop/logfile/firefox.log"
+profile["webdriver.log.file"] = "/Users/z/Desktop/firefox.log"
 
 driver = Selenium::WebDriver.for :firefox, profile: profile
 driver.get "http://en.wikipedia.beta.wmflabs.org/wiki/Main_Page"
